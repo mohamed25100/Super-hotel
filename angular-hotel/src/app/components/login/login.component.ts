9e695b331752cd92d7a15350f40b89c5c1eb75f4
Mohamed BOUCHERBA <mboucherba31@gmail.com>
2025-02-25 16:00:21 +0100

    create token service & update login component

--- angular-hotel/src/app/components/login/login.component.ts
+++ angular-hotel/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ICredential } from 'src/app/model/credential';
 import { Role, User } from 'src/app/model/user.model';
 import { AuthenticateService } from 'src/app/services/authenticate.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit {
   user: User | undefined;
   errorMessage: string | undefined;
 
-  constructor(public authService: AuthenticateService,private router: Router) {
+  constructor(public authService: AuthenticateService, private router: Router, private tokenService: TokenService) {
 
   }
 
@@ -32,14 +33,62 @@ export class LoginComponent implements OnInit {
     );
   }
 
+  /**
+   * Méthode de connexion de l'utilisateur
+   */
   onLogin(): void {
-    console.log("Connexion " + this.myForm.email + " " + this.myForm.password);
-  
+    console.log("Tentative de connexion pour :", this.myForm.email);
+
     this.authService.login(this.myForm).subscribe(
-      
-    )
+      (data) => {
+        if (!data["access-token"]) {
+          console.error("⚠️ Aucun token reçu !");
+          this.errorMessage = "Erreur de connexion. Aucun token reçu.";
+          return;
+        }
+
+        // Sauvegarde du token et récupération des informations de l'utilisateur
+        this.tokenService.saveToken(data["access-token"]);
+        let userInfo = this.tokenService.getPayload();
+        console.log("Utilisateur après décodage :", userInfo);
+
+        if (userInfo.email && userInfo.role) {
+          this.authService.setUser(userInfo);
+          this.user = new User(
+            userInfo.id,
+            userInfo.firstName,
+            userInfo.lastName,
+            userInfo.email,
+            "",
+            userInfo.role as Role
+          );
+
+          // Redirection en fonction du rôle
+          switch (userInfo.role) {
+            case Role.ADMIN:
+              this.router.navigate(['/admin']);
+              break;
+            case Role.MANAGER:
+              this.router.navigate(['/manager-dashboard']);
+              break;
+            default:
+              this.router.navigate(['/home']);
+          }
+        } else {
+          console.error("Erreur : Impossible de récupérer les infos utilisateur.");
+          this.errorMessage = "Erreur de connexion.";
+        }
+      },
+      (err) => {
+        console.error("Erreur lors de la connexion :", err);
+        this.errorMessage = "Identifiants incorrects ou problème serveur.";
+      }
+    );
   }
 
+  /**
+ * Redirige vers la page d'accueil
+ */
   goToHome(): void {
     this.router.navigateByUrl('/');
   }
---
+++ angular-hotel/src/app/services/token.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ITokenUser, Role } from '../model/user.model';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor(private router: Router) { }
+
+  saveToken(token: string) {
+    console.log("Token sauvegardé : ", token);
+    localStorage.setItem('token', token);
+  }
+
+    /**
+   * Vérifie si un utilisateur est connecté en regardant s'il y a un token.
+   * @returns true si un token est présent, sinon false.
+   */
+  isLogged(): boolean {
+    return localStorage.getItem('token') !== null;
+  }
+
+    /**
+   * Supprime le token et redirige vers la page d'accueil.
+   */
+  clearToken() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
+  }
+
+    /**
+   * Supprime le token en cas d'expiration et redirige vers la page de connexion.
+   */
+  clearTokenExpired(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+    /**
+   * Récupère le token stocké dans le localStorage.
+   * @returns Le token s'il existe, sinon null.
+   */
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getPayload(): any {
+    let userConnected: ITokenUser = {
+      id: 0,
+      firstName: '',
+      lastName: '',
+      email: '',
+      role: ''
+    };
+
+    let token = this.getToken();
+
+    if (token) {
+      console.log("Token avant décodage:", token);
+
+      try {
+        if (token.split('.').length !== 3) {
+          throw new Error("Token mal formé");
+        }
+
+        const decoded: any = jwtDecode(token);
+        console.log("Token décodé:", decoded);
+
+        userConnected.id = decoded.id || 0;
+        userConnected.firstName = decoded.firstName || '';
+        userConnected.lastName = decoded.lastName || '';
+        userConnected.email = decoded.email || '';
+        userConnected.role = this.extractRole(decoded.scope);
+        
+      }catch (error) {
+        console.error("Erreur lors du décodage du token :", error);
+        this.clearToken();
+      }
+    }
+    return userConnected;
+  }
+
+    /**
+   * Extrait le rôle de l'utilisateur depuis le token.
+   * @param scope - La valeur du champ `scope` dans le token.
+   * @returns Le rôle sous forme de chaîne de caractères.
+   */
+    private extractRole(scope: string): string {
+      if (!scope) return '';
+  
+      if (scope.includes(Role.ADMIN)) return Role.ADMIN;
+      if (scope.includes(Role.MANAGER)) return Role.MANAGER;
+      if (scope.includes(Role.USER)) return Role.USER;
+  
+      return '';
+    }
+}
